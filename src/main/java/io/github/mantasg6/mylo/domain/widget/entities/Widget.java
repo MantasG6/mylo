ee20a4bcaff92538e522e517931b1871e7f0355c
Mantas Gricius <mantasgricius6@gmail.com>
2026-06-25 21:27:49 +0300

    finish up with workspace and widget entities

--- src/main/java/io/github/mantasg6/mylo/domain/widget/entities/Widget.java
+++ src/main/java/io/github/mantasg6/mylo/domain/widget/entities/Widget.java
@@ -1,16 +1,28 @@
-package io.github.mantasg6.mylo.domain.entities;
+package io.github.mantasg6.mylo.domain.widget.entities;
 
 import io.github.mantasg6.mylo.core.database.BaseEntity;
+import io.github.mantasg6.mylo.domain.workspace.entities.Workspace;
 import io.github.mantasg6.mylo.shared.enums.WidgetType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "widgets")
 public class Widget extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "workspace_id", nullable = false)
+    private Workspace workspace;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "target_type")
---
+++ src/main/java/io/github/mantasg6/mylo/domain/workspace/entities/Workspace.java
@@ -0,0 +1,26 @@
+package io.github.mantasg6.mylo.domain.workspace.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.mantasg6.mylo.core.database.BaseEntity;
+import io.github.mantasg6.mylo.domain.widget.entities.Widget;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "workspaces")
+public class Workspace extends BaseEntity {
+
+    private String name;
+
+    @OneToMany(mappedBy = "workspace", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Widget> widgets = new ArrayList<>();
+
+}
