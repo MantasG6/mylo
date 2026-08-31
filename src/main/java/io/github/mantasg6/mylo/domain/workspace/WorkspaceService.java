00b93192e0cc874a21b27b63f1e69742ee84d782
Mantas Gricius <mantasgricius6@gmail.com>
2026-08-31 21:44:53 +0300

    add comments

--- src/main/java/io/github/mantasg6/mylo/domain/workspace/WorkspaceService.java
+++ src/main/java/io/github/mantasg6/mylo/domain/workspace/WorkspaceService.java
@@ -18,23 +18,47 @@ public class WorkspaceService {
     private final WorkspaceRepository workspaceRepository;
     private final WorkspaceMapper workspaceMapper;
 
+    /**
+     * Get all workspaces from the database.
+     *
+     * @return List of all workspaces.
+     */
     public List<WorkspaceResponse> getAllWorkspaces() {
         return workspaceRepository.findAll().stream()
                 .map(workspaceMapper::toDto)
                 .toList();
     }
 
+    /**
+     * Get a single workspace by id.
+     *
+     * @param id ID of the workspace.
+     * @return Workspace details of the workspace with the provided id.
+     */
     public WorkspaceResponse getWorkspaceById(Long id) {
         Workspace workspace = workspaceRepository.findById(id)
                 .orElseThrow(() -> new WorkspaceNotFoundException(id));
         return workspaceMapper.toDto(workspace);
     }
 
+    /**
+     * Creates a new workspace with the details provided in the request.
+     *
+     * @param request Workspace request containing all the details for the new workspace.
+     * @return Workspace response containing all the details of the new workspace.
+     */
     public WorkspaceResponse createWorkspace(WorkspaceRequest request) {
         Workspace newWorkspace = workspaceMapper.toEntity(request);
         return workspaceMapper.toDto(workspaceRepository.save(newWorkspace));
     }
 
+    /**
+     * Updates workspace with the provided id.
+     *
+     * @param id ID of the workspace to update.
+     * @param request Workspace details to be updated.
+     * @return New workspace details.
+     */
     public WorkspaceResponse updateWorkspace(Long id, WorkspaceRequest request) {
         Workspace workspace = workspaceRepository.findById(id).orElseThrow(() -> new WorkspaceNotFoundException(id));
 
@@ -45,6 +69,11 @@ public class WorkspaceService {
         return workspaceMapper.toDto(workspaceRepository.save(workspace));
     }
 
+    /**
+     * Deletes the workspace with provided id.
+     *
+     * @param id ID of the workspace to delete.
+     */
     public void deleteWorkspace(Long id) {
         workspaceRepository.deleteById(id);
     }
