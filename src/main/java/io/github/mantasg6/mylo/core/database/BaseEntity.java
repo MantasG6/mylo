b4bb4bd12d336af618cbe05fa99f9b4d6ef8ae2a
Mantas Gricius <mantasgricius6@gmail.com>
2026-09-04 23:05:16 +0300

    add auditing to autogenerate createdAt and updatedAt

--- src/main/java/io/github/mantasg6/mylo/core/database/BaseEntity.java
+++ src/main/java/io/github/mantasg6/mylo/core/database/BaseEntity.java
@@ -5,8 +5,10 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,6 +23,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
---
+++ src/main/java/io/github/mantasg6/mylo/core/database/JpaConfig.java
@@ -0,0 +1,8 @@
+package io.github.mantasg6.mylo.core.database;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
