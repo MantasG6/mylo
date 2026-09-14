7eea47dd5412a79f7d77b55af7c27ff867944694
Mantas Gricius <mantasgricius6@gmail.com>
2026-09-14 22:22:37 +0300

    add comment

--- src/main/java/io/github/mantasg6/mylo/domain/widget/WidgetPositionException.java
+++ src/main/java/io/github/mantasg6/mylo/domain/widget/WidgetPositionException.java
@@ -2,6 +2,10 @@ package io.github.mantasg6.mylo.domain.widget;
 
 import io.github.mantasg6.mylo.core.exception.CommonApplicationException;
 
+/**
+ * Exception thrown when Widget position is already taken.
+ *
+ */
 public class WidgetPositionException extends CommonApplicationException {
 
     public WidgetPositionException(Long workspaceId, Integer position) {
